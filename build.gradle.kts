e06f7d44c9cf4b8db7b9978970386edc1a434ae0
Marek Osvald <marek.osvald@kiwi.com>
2024-01-19 18:17:37 +0100

    Fix Gradle DSL, update Kotlin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.3.50"
+    kotlin("jvm") version "1.9.22"
 }
 
 group = "cz.drekorian"
@@ -19,11 +19,11 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    compile("com.github.jkcclemens", "khttp", "-SNAPSHOT")
-    compile("io.github.microutils:kotlin-logging:1.12.5")
-    compile("org.slf4j", "slf4j-simple", "1.7.36")
-    compile("org.jsoup", "jsoup", "1.11.3")
-    testCompile("junit", "junit", "4.12")
+    implementation("com.github.jkcclemens", "khttp", "-SNAPSHOT")
+    implementation("io.github.microutils:kotlin-logging:1.12.5")
+    implementation("org.slf4j", "slf4j-simple", "1.7.36")
+    implementation("org.jsoup", "jsoup", "1.11.3")
+    testImplementation("junit", "junit", "4.12")
 }
 
 configure<JavaPluginConvention> {
@@ -35,30 +35,3 @@ configure<JavaPluginConvention> {
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
-
-tasks.withType<Jar> {
-    archiveName = "avon-fetcher.jar"
-    manifest.attributes.put("Main-Class", "cz.drekorian.avonfetcher.Main")
-    from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
-}
-
-//jar {
-//    archiveName = "avon-fetcher.jar"
-//
-//    from {
-//
-//        configurations.runtime.collect {
-//            it.isDirectory() ? it : zipTree(it)
-//        }
-//
-//        configurations.compile.collect {
-//            it.isDirectory() ? it : zipTree(it)
-//        }
-//    }
-//
-//    manifest {
-//        attributes 'Main-Class': 'cz.drekorian.avonfetcher'
-//    }
-
-//    exclude 'META-INF/*.RSA', 'META-INF/*.SF','META-INF/*.DSA'
-//}
